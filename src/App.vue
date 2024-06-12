3726ebd6528c8f52c90536152a05e471f90f5350
DaniDeveloper11 <angel.montes0498@gmail.com>
2024-06-11 19:13:24 -0600

    merge from miguels branch

--- src/App.vue
+++ src/App.vue
@@ -135,13 +135,16 @@
                   </span>
                 </MenuButton>
               </div>
-              <TransitionRoot as="template" enter="transition ease-out duration-100" enter-from="transform opacity-0 scale-95" enter-to="transform opacity-100 scale-100" leave="transition ease-in duration-75" leave-from="transform opacity-100 scale-100" leave-to="transform opacity-0 scale-95">
-                <MenuItems class="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <MenuItem v-for="item in userNavigation" :key="item.name">
-                    <a :href="item.href" class="block px-4 py-2 text-sm text-gray-700">{{ item.name }}</a>
+              <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                <MenuItems class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                  <MenuItem v-slot="{ active }" class="cursor-pointer">
+                    <span v-on:click="profileOpen = true" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']">Perfil</span>
+                  </MenuItem>
+                  <MenuItem v-slot="{ active }" class="cursor-pointer"> 
+                    <span v-on:click="logout" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']">Salir</span>
                   </MenuItem>
                 </MenuItems>
-              </TransitionRoot>
+              </transition>
             </Menu>
           </div>
         </div>
