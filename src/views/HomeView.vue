fafd240d0f20cc23969e90b1e79badefd4e81094
DaniDeveloper11 <angel.montes0498@gmail.com>
2024-06-14 12:43:25 -0600

    just added styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,13 @@
         <div class="overflow-hidden">
           <div class="mx-auto max-w-7xl px-6 pb-32 pt-0 sm:pt-0 lg:px-8 lg:pt-0">
             <div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
-              <div class="flex w-full">
-                <div class="w-full max-w-xl lg:shrink-0 xl:max-w-2xl flex-1">
-                  <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Azul ERP</h1>
-                  <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                    Es una solución integral específica para la industria tequilera, proporcionando una plataforma unificada para gestionar todos los aspectos operativos, desde el control de gastos hasta la generación de órdenes de venta y reportes detallados. Está diseñado para mejorar la eficiencia, precisión y toma de decisiones estratégicas, asegurando la sincronización en cada etapa de la producción y distribución del tequila.
-                  </p>
-                </div>
+              <div class="w-full sm:flex">
+                <div class="w-full max-w-xl lg:shrink-0 xl:max-w-2xl ">
+                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Azul ERP.</h1>
+                <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">Es una solución integral específica para la industria tequilera, proporcionando una plataforma 
+                    unificada para gestionar todos los aspectos operativos, desde el control de gastos hasta la
+                     generación de órdenes de venta y reportes detallados. Está diseñado para mejorar la eficiencia, precisión y toma de decisiones estratégicas, asegurando la sincronización en cada etapa de la producción y distribución del tequila.</p>
+              </div>
                 <div class="flex-1 mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                   <div class="ml-auto w-44 flex-none space-y-0 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                     <div class="relative">
