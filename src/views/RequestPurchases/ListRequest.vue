e042ef9d138db2bc45d94e10f71b69daab503a84
KenetGaona115 <aldrinkenet@hotmail.com>
2024-07-02 19:32:30 -0600

    pdf issue

--- src/views/RequestPurchases/ListRequest.vue
+++ src/views/RequestPurchases/ListRequest.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        <h4 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{{ returnTitle() }}</h4>
+        <!-- <h4 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{{ returnTitle() }}</h4> -->
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-base font-semibold leading-6 text-gray-900">{{ returnTitle() }}</h1>
+                    <p class="mt-2 text-sm text-gray-700">
+                        Este módulo permite a los usuarios gestionar y realizar un seguimiento de las solicitudes de
+                        compra dentro de la organización.
+                        Los usuarios pueden ver todas las solicitudes de compra en curso, incluyendo detalles como el
+                        artículo solicitado,
+                        el costo estimado y el estado actual de la solicitud (aprobado o rechazado).
+                    </p>
+                </div>
+            </div>
+        </div>
         <ul role="list" class="divide-y divide-gray-200" v-if="hasData">
             <li v-for="req in request" :key="req.id" v-if="request.length > 0" class="px-4 py-4 sm:px-0">
                 <div class="flex justify-between items-center">
@@ -19,71 +33,136 @@
                         <p class="text-sm text-gray-500">{{ moneyFormatter(req.docTotal) }}</p>
                     </div>
                     <div class="ml-4">
-                        <span v-if="req.docStatus == 0"
+                        <span v-if="req.docStatus == 1"
+                            class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-4 bg-yellow-100 text-yellow-600">
+                            PENDIENTE
+                        </span>
+                        <span v-if="req.docStatus == 2"
                             class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-4 bg-green-100 text-green-600">
                             APROBADO
                         </span>
-                        <span v-if="req.docStatus != 0"
+                        <span v-if="req.docStatus == 3"
                             class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-4 bg-red-100 text-red-600">
                             RECHAZADO
                         </span>
+                        <span v-if="req.docStatus == 4"
+                            class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-4 bg-red-100 text-red-600">
+                            CANCELADO
+                        </span>
+                        <span v-if="req.docStatus == 5"
+                            class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-4 bg-blue-100 text-blue-600">
+                            IMPRESO
+                        </span>
+                        <span v-if="req.docStatus == 6"
+                            class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-4 bg-gray-100 text-gray-600">
+                            CERRADO
+                        </span>
                     </div>
                 </div>
-
-                <!-- <div class="mt-8 flow-root" v-if="req.showItems">
-                    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                            <table class="min-w-full divide-y divide-gray-300">
-                                <thead>
-                                    <tr>
-                                        <th scope="col"
-                                            class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                                        </th>
-                                        <th scope="col"
-                                            class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                            Articulo</th>
-                                        <th scope="col"
-                                            class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                            Descripcion</th>
-                                        <th scope="col"
-                                            class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                            Precio</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="divide-y divide-gray-200 bg-white">
-                                    <tr v-for="item in req.items" :key="item.id">
-                                        <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0 text-center align-middle">{{
-                                            "x" + item.quantity }}</td>
-                                        <td class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">{{
-                                            item.article }}</td>
-                                        <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">{{
-                                            item.description }}</td>
-                                        <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{
-                                            moneyFormatter(item.price) }}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div> -->
             </li>
         </ul>
         <div class="noHasData" v-if="!hasData">
             <p class="mt-6 text-xl leading-8 text-gray-700">No hay resultados</p>
         </div>
 
-        <RequestDetails v-if="showDetails" :data="itemSelected" @closeModal="handleClose"></RequestDetails>
+        <RequestDetails v-if="showDetails" :data="itemSelected" @closeModal="handleClose" @downloadPDF="getPDF">
+        </RequestDetails>
 
+        <div v-if="false" ref="pdfContent" class="pdf-content">
+            <div class="text-center">
+                <a href="/"><img src="../../assets/logo.png" width="150" alt="Logo" class="logo"></a>
+            </div>
+            <div class="border-t border-gray-100">
+                <dl class="divide-y divide-gray-100">
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Departamento</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ itemSelected.department }}
+                        </dd>
+                    </div>
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Concepto</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ itemSelected.concept }}
+                        </dd>
+                    </div>
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Beneficiario</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ itemSelected.beneficiary }}
+                        </dd>
+                    </div>
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Total</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ moneyFormatter(itemSelected.docTotal) }}
+                        </dd>
+                    </div>
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Comentarios</dt>
+                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {{ itemSelected.comments }}
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+            <div class="flow-root">
+                <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead>
+                                <tr>
+                                    <th scope="col"
+                                        class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                    </th>
+                                    <th scope="col"
+                                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Articulo
+                                    </th>
+                                    <th scope="col"
+                                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Descripcion
+                                    </th>
+                                    <th scope="col"
+                                        class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Precio
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white">
+                                <tr v-for="item in itemSelected.items" :key="item.id">
+                                    <td
+                                        class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0 text-center align-middle">
+                                        {{ "x" + item.quantity }}
+                                    </td>
+                                    <td class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                                        {{ item.article }}
+                                    </td>
+                                    <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
+                                        {{ item.description }}
+                                    </td>
+                                    <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                                        {{ moneyFormatter(item.price) }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import axios from '../../utils/axios.js';
 import Swal from 'sweetalert2';
 import RequestDetails from './modals/RequestDetails.vue';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 export default {
     components: {
-        RequestDetails
+        RequestDetails,
     },
     data() {
         return {
@@ -92,7 +171,9 @@ export default {
             token: localStorage.getItem('token'),
             hasData: true,
             showDetails: false,
-            itemSelected: null
+            itemSelected: null,
+            filename: "Solicitud de compra.pdf",
+            showPdfContent: false
         }
     },
     created() {
@@ -171,6 +252,34 @@ export default {
         },
         handleClose() {
             this.showDetails = false;
+            this.itemSelected = null;
+        },
+        getPDF() {
+            this.showDetails = false; // Mostrar el contenido para PDF
+            this.$nextTick(() => {
+                const pdfContent = this.$refs.pdfContent;
+                console.log('pdfContent:', pdfContent);
+
+                if (!pdfContent) {
+                    console.error("No se encontró el contenido del PDF.");
+                    return;
+                }
+
+                setTimeout(() => {
+                    html2canvas(pdfContent).then((canvas) => {
+                        const imgData = canvas.toDataURL('image/png');
+                        const pdf = new jsPDF({
+                            orientation: 'landscape',
+                            unit: 'px',
+                            format: [canvas.width, canvas.height]
+                        });
+                        pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
+                        pdf.save('Orden_Compra.pdf'); // Ocultar el contenido del PDF después de generar el PDF
+                    }).catch((error) => {
+                        console.error("Error generando el PDF:", error); // Asegurarse de ocultar el contenido en caso de error
+                    });
+                }, 500); // retraso para asegurar que el contenido esté completamente renderizado
+            });
             this.itemSelected = null;
         }
     }
--- src/views/RequestPurchases/modals/RequestDetails.vue
+++ src/views/RequestPurchases/modals/RequestDetails.vue
@@ -15,7 +15,44 @@
                         leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                         <DialogPanel
                             class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                            <div class="mt-8 flow-root">
+                            <div class="border-t border-gray-100">
+                                <dl class="divide-y divide-gray-100">
+                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                        <dt class="text-sm font-medium leading-6 text-gray-900">Departamento</dt>
+                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {{ data.department }}
+                                        </dd>
+                                    </div>
+                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                        <dt class="text-sm font-medium leading-6 text-gray-900">Concepto</dt>
+                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {{ data.concept }}
+                                        </dd>
+                                    </div>
+                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                        <dt class="text-sm font-medium leading-6 text-gray-900">Beneficiario</dt>
+                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {{ data.beneficiary }}
+                                        </dd>
+                                    </div>
+                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                        <dt class="text-sm font-medium leading-6 text-gray-900">Total
+                                        </dt>
+                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {{ moneyFormatter(data.docTotal) }}
+                                        </dd>
+                                    </div>
+                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                                        <dt class="text-sm font-medium leading-6 text-gray-900">Comentarios</dt>
+                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {{ data.comments }}
+                                        </dd>
+                                    </div>
+                                </dl>
+                            </div>
+
+                            <!-- Botones para cerrar el modal -->
+                            <div class="flow-root">
                                 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                                     <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                                         <table class="min-w-full divide-y divide-gray-300">
@@ -60,14 +97,15 @@
                                     </div>
                                 </div>
                             </div>
+                            <!-- Botones para cerrar el modal -->
                             <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                                 <button type="button"
                                     class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
-                                    @click="closeModal">Deactivate
+                                    @click="pdfDownload">Descargar PDF
                                 </button>
                                 <button type="button"
                                     class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                                    @click="closeModal">Cancel
+                                    @click="closeModal">Cerrar
                                 </button>
                             </div>
                         </DialogPanel>
@@ -90,11 +128,16 @@ const props = defineProps({
     }
 })
 
-const emit = defineEmits(['closeModal'])
+const emit = defineEmits(['closeModal', 'downloadPDF'])
 
 const closeModal = () => {
     open.value = false
     emit('closeModal', 'Datos desde el hijo')
+}
+
+const pdfDownload = () => {
+    open.value = false
+    emit('downloadPDF', 'Datos desde el hijo')
 }
 
 const moneyFormatter = new Intl.NumberFormat('es-mx', {
