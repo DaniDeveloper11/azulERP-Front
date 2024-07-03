7c00ee516c164aa114a351d5e00a53a4c8398de4
DaniDeveloper11 <angel.montes0498@gmail.com>
2024-07-02 20:12:54 -0600

    listo

--- src/components/tableRequests.vue
+++ src/components/tableRequests.vue
@@ -43,7 +43,7 @@
             <div class="flex flex-none items-center gap-x-4">
           
                 <button @click="open = true; requestModal = request"
-                    class=" rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block">
+                    class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block">
                     Revisar
                 </button>
 
