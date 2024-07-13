e94106173528dc29fa5c01253547a4eb349c29c7
DaniDeveloper11 <angel.montes0498@gmail.com>
2024-07-12 23:55:38 -0600

    fix: lisbox

--- src/views/Users/AddUserView.vue
+++ src/views/Users/AddUserView.vue
@@ -143,7 +143,7 @@
           <div class="sm:col-span-2">
             <label for="region" class="block text-sm font-medium leading-6 text-gray-900">Estado</label>
             <div class="mt-2">
-              <select v-model="user_state" class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+              <select v-model="user_state" required class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                 <option value="" disabled selected>Seleccione un estado</option>
                 <option v-for="estado in estados" :key="estado.abreviatura" :value="estado.abreviatura">
                   {{ estado.nombre }}
@@ -183,7 +183,7 @@
             <label for="department"
               class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Departamento</label>
             <div class="mt-2 sm:col-span-2 sm:mt-0">
-              <select v-model="user_department" id="department" name="department"
+              <select v-model="user_department" id="department" name="department" required
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
                 <option v-for="depa in departments" :key="depa.id" :value='depa.id'> {{ depa.name }}</option>
               </select>
