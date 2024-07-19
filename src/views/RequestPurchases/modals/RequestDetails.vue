2becd3b546fce13a591d73fb2f2f59d2055267d2
DaniDeveloper11 <angel.montes0498@gmail.com>
2024-07-18 22:28:37 -0600

    fix: request details

--- src/views/RequestPurchases/modals/RequestDetails.vue
+++ src/views/RequestPurchases/modals/RequestDetails.vue
@@ -20,7 +20,7 @@
                                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                                         <dt class="text-sm font-medium leading-6 text-gray-900">Departamento</dt>
                                         <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            {{ data.department }}
+                                            {{ data.department.name }}
                                         </dd>
                                     </div>
                                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -32,7 +32,7 @@
                                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                                         <dt class="text-sm font-medium leading-6 text-gray-900">solicitante</dt>
                                         <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            {{ data.userRequest }}
+                                            {{ data.userRequest.name }} {{ data.userRequest.lastname }} 
                                         </dd>
                                     </div>
                                     <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
