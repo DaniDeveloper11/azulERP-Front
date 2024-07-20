fe166486e94289440a55c2ac6e1c2a90ef8efc2c
Leonardo Talavera <leosistema281@gmail.com>
2024-07-20 09:27:21 -0600

    Bug fix in Preview Order

--- src/views/ApproveRequest/Ordenes/PreviewOrder.vue
+++ src/views/ApproveRequest/Ordenes/PreviewOrder.vue
@@ -23,7 +23,7 @@
           <div class="mt-2 sm:max-w-md">
             <div
               class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-              <select v-model="orderData.subdepartment.id" id="subdepartment"
+              <select v-model="orderData.department.subdepartment" id="subdepartment"
                 class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 <option v-for="subdept in subdepartments" :key="subdept.id" :value="subdept.id">{{ subdept.name }}
                 </option>
